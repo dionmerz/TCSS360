f25da6ee58d0a79d1c589f9b29e7b6c6f5c52af5
bernabeg <bernabeg@uw.edu>
2016-05-07 14:44:43 -0700

    Conference Junit test started

---
+++ ConferenceSystem/src/jUnitTests/ConferenceTest.java
@@ -0,0 +1,26 @@
+package jUnitTests;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import TCSS360.Author;
+import TCSS360.Conference;
+import TCSS360.Paper;
+import TCSS360.User;
+
+public class ConferenceTest {
+	private List<Conference> confList;
+	private Paper script;
+	private Paper script2;
+	private User usr;
+	private Author auth;
+	private Conference conf;
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
